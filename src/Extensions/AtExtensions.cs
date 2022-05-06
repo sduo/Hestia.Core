b13f552243c3bc98b29bb6c346244000df7b5f95
suduo <sduo@vip.qq.com>
2022-05-06 11:59:41 +0800

    AtExtensions

---
+++ src/Extensions/AtExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Hestia.Core
+{
+    public static class AtClassExtensions
+    {
+        public static T At<T>(this IList<T> list, int index=0) where T : class 
+        {
+            if(list == null) { return null; }
+            if (list.Count == 0) { return  null; }
+            if (index < 0) { index += list.Count; }
+            if (index < 0 || index >= list.Count) { return null; }
+            return list[index];
+        }
+
+        public static T At<T>(this T[] array,int index =0) where T : class
+        {
+            if(array == null) { return null; }
+            if(array.Length == 0) { return null; }
+            if (index < 0) { index += array.Length; }
+            if (index < 0 || index >= array.Length) { return null; }
+            return array[index];
+        }
+    }
+
+    public static class AtStructExtensions
+    {
+        public static T? At<T>(this IList<T> list, int index = 0) where T : struct
+        {
+            if (list == null) { return null; }
+            if (list.Count == 0) { return null; }
+            if (index < 0) { index += list.Count; }
+            if (index < 0 || index >= list.Count) { return null; }
+            return list[index];
+        }
+
+        public static T? At<T>(this T[] array, int index = 0) where T : struct
+        {
+            if (array == null) { return null; }
+            if (array.Length == 0) { return null; }
+            if (index < 0) { index += array.Length; }
+            if (index < 0 || index >= array.Length) { return null; }
+            return array[index];
+        }
+    }
+}
---
+++ tests/Extensions/IListExtensions/At.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hestia.Core.Tests.Extensions.IListExtensions
+{
+    [TestClass]
+    public sealed class At
+    {
+        internal List<string> nullClassList = null;
+        internal List<int> nullStructList = null;
+        internal string[] nullClassArray = null;
+        internal int[] nullStructArray = null;
+        internal List<string> emptyClassList = new List<string>();
+        internal List<int> emptyStructList = new List<int>();
+        internal string[] emptyClassArray = new string[0];
+        internal int[] emptyStructArray = new int[0];
+        internal List<string> classList =new List<string>() { "A","B","C" };
+        internal List<int> structList = new List<int>() { 0,1,2 };
+        internal string[] classArray = new string[] { "A","B","C" };
+        internal int[] structArray = new int[] { 0,1,2 };
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual("A", classList.At(0));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("A", classList.At(-3));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual("C", classList.At(2));
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Assert.AreEqual("C", classList.At(-1));
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            Assert.AreEqual(null, classList.At(3));
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            Assert.AreEqual(null, classList.At(-4));
+        }
+
+        [TestMethod]
+        public void Test7()
+        {
+            Assert.AreEqual(0, structList.At(0));
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+            Assert.AreEqual(0, structList.At(-3));
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            Assert.AreEqual(2, structList.At(2));
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            Assert.AreEqual(2, structList.At(-1));
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+            Assert.AreEqual(null, structList.At(3));
+        }
+
+        [TestMethod]
+        public void Test12()
+        {
+            Assert.AreEqual(null, structList.At(-4));
+        }
+
+        [TestMethod]
+        public void Test13()
+        {
+            Assert.AreEqual("A", classArray.At(0));
+        }
+
+        [TestMethod]
+        public void Test14()
+        {
+            Assert.AreEqual("A", classArray.At(-3));
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+            Assert.AreEqual("C", classArray.At(2));
+        }
+
+        [TestMethod]
+        public void Test16()
+        {
+            Assert.AreEqual("C", classArray.At(-1));
+        }
+
+        [TestMethod]
+        public void Test17()
+        {
+            Assert.AreEqual(null, classArray.At(3));
+        }
+
+        [TestMethod]
+        public void Test18()
+        {
+            Assert.AreEqual(null, classArray.At(-4));
+        }
+
+        [TestMethod]
+        public void Test19()
+        {
+            Assert.AreEqual(0, structArray.At(0));
+        }
+
+        [TestMethod]
+        public void Test20()
+        {
+            Assert.AreEqual(0, structArray.At(-3));
+        }
+
+        [TestMethod]
+        public void Test21()
+        {
+            Assert.AreEqual(2, structArray.At(2));
+        }
+
+        [TestMethod]
+        public void Test22()
+        {
+            Assert.AreEqual(2, structArray.At(-1));
+        }
+
+        [TestMethod]
+        public void Test23()
+        {
+            Assert.AreEqual(null, structArray.At(3));
+        }
+
+        [TestMethod]
+        public void Test24()
+        {
+            Assert.AreEqual(null, structArray.At(-4));
+        }
+
+        [TestMethod]
+        public void Test25()
+        {
+            Assert.AreEqual(null, nullClassList.At());
+        }
+
+        [TestMethod]
+        public void Test26()
+        {
+            Assert.AreEqual(null, nullStructList.At());
+        }
+
+        [TestMethod]
+        public void Test27()
+        {
+            Assert.AreEqual(null, nullClassArray.At());
+        }
+
+        [TestMethod]
+        public void Test28()
+        {
+            Assert.AreEqual(null, nullStructArray.At());
+        }
+
+        [TestMethod]
+        public void Test29()
+        {
+            Assert.AreEqual(null, emptyClassList.At());
+        }
+
+        [TestMethod]
+        public void Test30()
+        {
+            Assert.AreEqual(null, emptyStructList.At());
+        }
+
+        [TestMethod]
+        public void Test31()
+        {
+            Assert.AreEqual(null, emptyClassArray.At());
+        }
+
+        [TestMethod]
+        public void Test32()
+        {
+            Assert.AreEqual(null, emptyStructArray.At());
+        }
+    }
+}
