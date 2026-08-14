02189cde49762e571f1e95a5247a1fe24e4cb8df
SDuo <sduo@vip.qq.com>
2026-08-14 17:11:56 +0800

    update

--- src/Cache/Store.cs
+++ src/Cache/Store.cs
@@ -8,16 +8,16 @@ namespace Hestia.Core.Cache
 {
     public abstract class Store<T, R>(TimeSpan timeout) : IReadOnlyCollection<T>
     {
-        protected volatile List<T> store = [];
+        protected volatile T[] store = null;
         private readonly SemaphoreSlim @lock = new(1, 1);
         protected TimeSpan Timeout { get; } = timeout;
 
         public DateTimeOffset UpdateUtc { get; protected set; } = DateTimeOffset.MinValue;
 
         #region IReadOnlyCollection
-        public int Count => store.Count;
+        public int Count => store.Length;
 
-        public IEnumerator<T> GetEnumerator() => store.GetEnumerator();
+        public IEnumerator<T> GetEnumerator() => (store as IEnumerable<T>).GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => store.GetEnumerator();
         #endregion
